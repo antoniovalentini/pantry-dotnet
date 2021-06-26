188f9451a6d6fdfcbaea46ab95ba997a820f6af1
antoniovalentini <antonio.valentini@live.it>
2021-06-26 21:14:34 +0200

    Fix tests configuration

---
+++ test/Pantry.Core.E2ETests/Baskets/BasketsClientTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Pantry.Core.Baskets;
+using Xunit;
+
+namespace Pantry.Core.E2ETests.Baskets
+{
+    public class BasketsClientTests
+    {
+        private readonly PantrySettings _pantrySettings;
+        private readonly ApiClient _apiclient;
+
+        public BasketsClientTests()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json", false)
+                .Build();
+
+            var testSettings = config.GetSection("Pantry").Get<PantryTestSettings>();
+            _pantrySettings = new PantrySettings {PantryId = testSettings.Id};
+            _apiclient = new ApiClient(_pantrySettings);
+        }
+
+        [Fact]
+        public async Task CreateBasket_ShouldSucceed()
+        {
+            var client = new BasketsClient(_apiclient, _pantrySettings);
+            var ex = await Record.ExceptionAsync(async () => await client.CreateBasket("test3-basket", new TestObject()));
+            Assert.Null(ex);
+        }
+    }
+
+    public class TestObject
+    {
+        public string TestProp1 { get; set; } = "foo";
+        public string TestProp2 { get; set; } = "bar";
+    }
+}
--- test/Pantry.Core.E2ETests/Pantry/PantryClientTests.cs
+++ test/Pantry.Core.E2ETests/Pantry/PantryClientTests.cs
@@ -1,44 +1,35 @@
-﻿using System.IO;
-using System.Text.Json;
-using Pantry.Core.Pantry;
+﻿using Pantry.Core.Pantry;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace Pantry.Core.E2ETests.Pantry
 {
     public class PantryClientTests
     {
+        private readonly PantrySettings _pantrySettings;
+        private readonly PantryTestSettings _testSettings;
+        private readonly ApiClient _apiclient;
+
+        public PantryClientTests()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json", false)
+                .Build();
+
+            _testSettings = config.GetSection("Pantry").Get<PantryTestSettings>();
+            _pantrySettings = new PantrySettings {PantryId = _testSettings.Id};
+            _apiclient = new ApiClient(_pantrySettings);
+        }
+
         [Fact]
         public async Task GetPantry_ShouldSucceed()
         {
-            var rawSettings = await File.ReadAllTextAsync("appsettings.test.json");
-            var testSettings = JsonSerializer.Deserialize<PantryTestSettings>(rawSettings);
-            Assert.NotNull(testSettings);
-            var settings = new PantrySettings {PantryId = testSettings.Id};
-
-            var apiclient = new ApiClient(settings);
-            var client = new PantryClient(apiclient, settings);
+            var client = new PantryClient(_apiclient, _pantrySettings);
             var pantry = await client.GetPantry();
             Assert.NotNull(pantry);
-            Assert.Equal(testSettings.Name, pantry.Name);
-            Assert.Equal(testSettings.Description, pantry.Description);
+            Assert.Equal(_testSettings.Name, pantry.Name);
+            Assert.Equal(_testSettings.Description, pantry.Description);
         }
-
-        [Fact]
-        public async Task CreateBasket_ShouldSucceed()
-        {
-            var settings = new PantrySettings();
-
-            var apiclient = new ApiClient(settings);
-            var client = new Baskets.BasketsClient(apiclient, settings);
-            var ex = await Record.ExceptionAsync(async () => await client.CreateBasket("test3-basket", new TestObject()));
-            Assert.Null(ex);
-        }
-    }
-
-    public class TestObject
-    {
-        public string TestProp1 { get; set; } = "foo";
-        public string TestProp2 { get; set; } = "bar";
     }
 }
